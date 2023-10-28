2782da93c1cdbf7590563d3e5dc93baa3b638ba2
Manuel Gómez <m.gomez49@uniandes.edu.co>
2023-10-28 12:07:56 -0500

    taller_funcional100%

--- src/app/serie/serie.component.ts
+++ src/app/serie/serie.component.ts
@@ -3,6 +3,11 @@ import { Serie } from './serie';
 import { dataSeries } from './dataSeries';
 import { SerieService } from "./serie.service";
 
+
+
+
+
+
 @Component({
   selector: 'app-serie',
   templateUrl: './serie.component.html',
@@ -10,6 +15,7 @@ import { SerieService } from "./serie.service";
 })
 export class SerieComponent implements OnInit {
   series: Array<Serie> = []
+  promedio: number = 0;
   constructor(private serieService: SerieService) { }
 
   getSeriesLis2(): Array<Serie> {
@@ -22,7 +28,24 @@ export class SerieComponent implements OnInit {
     });
   }
 
+  
+  
+
   ngOnInit() {
     this.getSeriesList();
   }
+  getSeasonAverage(series: Serie[]): number {
+    let totalSeasons: number = 0;
+    let numSeries: number = 0;
+    series.forEach((serie) => totalSeasons = totalSeasons + serie.seasons);
+    series.forEach((serie) => numSeries = numSeries + 1);
+    const promedio: number = totalSeasons / numSeries;
+    console.log(promedio);
+    return promedio;
+  }
 }
+
+
+
+
+
